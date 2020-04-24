08efeab6cc052b6a48118214f4a91f33e6810f81
sumit parmar <parmarsumit@gmail.com>
2020-04-24 16:40:45 +0100

    test

--- run_batch_from_dashboard/test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-color 0a
-mode 1000
-
-echo %random%%random% > C:\Users\siddh\Desktop\test.txt
--- run_batch_from_dashboard/test.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.call([r'D:\YouTube\SplunkHowTo\run_batch_file_from_dashboard\test.bat'])
